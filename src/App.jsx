325b30064cf101b5c6bffb206afa1f981451caf7
banucanturk <118526042+banucanturk@users.noreply.github.com>
2023-09-17 21:31:19 +0300

    projeye devam edilecek

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import './App.css';
+import Basket from './components/Basket';
+import Product from './components/Product';
+import productsData from './data/product';
+
+function App() {
+  const [basket, setBasket] = useState([]);
+  const [products, setProducts] = useState(productsData);
+
+  const addToBasket = (product) => {
+    const { id } = product;
+    const existingProductIndex = basket.findIndex((item) => item.id === id);
+
+    if (existingProductIndex !== -1) {
+      const updatedBasket = [...basket];
+      updatedBasket[existingProductIndex].quantity += 1;
+      setBasket(updatedBasket);
+    } else {
+      setBasket([...basket, { ...product, quantity: 1 }]);
+    }
+
+    // Stok durumunu guncelleme
+    const updatedProducts = products.map((p) => {
+      if (p.id === id && p.stock > 0) {
+        return {
+          ...p,
+          stock: p.stock - 1,
+        };
+      }
+      return p;
+    });
+    setProducts(updatedProducts);
+  };
+
+  const updateProductStock = (productId, newStock) => {
+    const updatedProducts = products.map((product) => {
+      if (product.id === productId) {
+        return {
+          ...product,
+          stock: newStock,
+        };
+      }
+      return product;
+    });
+    setProducts(updatedProducts);
+  };
+
+  return (
+    <div className="App">
+
+      <div className="main">
+        <h2>Ürünler</h2>
+        <div className="productList">
+          {products.map((product) => (
+            <Product
+              key={product.id}
+              product={product}
+              addToBasket={addToBasket}
+              updateProductStock={updateProductStock}
+            />
+          ))}
+        </div>
+        <Basket basket={basket} updateBasket={setBasket} products={products} setProducts={setProducts} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/product.js
@@ -0,0 +1,67 @@
+const products = [
+    {
+        id: 1,
+        title: "Cerrah",
+        price: 75,
+        img: "https://i.dr.com.tr/cache/500x400-0/originals/0001746383001-1.jpg",
+        stock: 5,
+    },
+    {
+        id: 2,
+        title: "Kırlangıç Çığlığı",
+        price: 92,
+        img: "https://i.dr.com.tr/cache/500x400-0/originals/0001843343001-1.jpg",
+        stock: 4,
+    },
+    {
+        id: 3,
+        title: "Aşkımız Eski Bir Roman",
+        price: 139,
+        img: "https://i.dr.com.tr/cache/500x400-0/originals/0001837961001-1.jpg",
+        stock: 10,
+    },
+    {
+        id: 4,
+        title: "İyi Bir Kızın Cinayet Rehberi",
+        price: 118,
+        img: "https://i.dr.com.tr/cache/500x400-0/originals/0001985877001-1.jpg",
+        stock: 10,
+    },
+    {
+        id: 5,
+        title: "Antika Titanik",
+        price: 59,
+        img: "https://i.dr.com.tr/cache/500x400-0/originals/0001776436001-1.jpg",
+        stock: 10,
+    },
+    {
+        id: 6,
+        title: "Beyoğlu Rapsodisi",
+        price: 79,
+        img: "https://i.dr.com.tr/cache/500x400-0/originals/0001852469001-1.jpg",
+        stock: 3,
+    },
+    {
+        id: 7,
+        title: "Tehlikeli Şarkılar",
+        price: 74,
+        img: "https://i.dr.com.tr/cache/500x400-0/originals/0002051348001-1.jpg",
+        stock: 4,
+    },
+    {
+        id: 8,
+        title: "Kızıl Nehirler",
+        price: 114,
+        img: "https://i.dr.com.tr/cache/500x400-0/originals/0000000104321-1.jpg",
+        stock: 6,
+    },
+    {
+        id: 9,
+        title: "Ölüler Diyarı",
+        price: 125,
+        img: "https://i.dr.com.tr/cache/500x400-0/originals/0001796089001-1.jpg",
+        stock: 9,
+    },
+];
+
+export default products;
